f1a3f01eda06a459ad0c39c76f0c5d2b46606385
Amine MEKKI <amine.mekki@mines-ales.org>
2023-09-14 21:54:37 +0200

    adding the rest of the pipeline

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.S1_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.S2_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.S3_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.S4_evaluation import ModelEvaluationPipeline
+from cnnClassifier.pipeline.S5_inferencer import ModelInferencePipeline
 
 
 
@@ -8,21 +11,54 @@ from cnnClassifier.pipeline.S2_prepare_base_model import PrepareBaseModelTrainin
 # testing 
 STAGE_NAME1 = "Data Ingestion"
 STAGE_NAME2 = "Prepare Base Model"
+STAGE_NAME3 = "Training"
+STAGE_NAME4 = "Evaluation"
+STAGE_NAME5 = "Inference"
+
 
 if __name__ == "__main__":
     try:
         logger.info(f">>>>> Stage {STAGE_NAME1} started <<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
+        data_ingestor = DataIngestionTrainingPipeline()
+        data_ingestor.main()
         logger.info(f">>>>> Stage {STAGE_NAME1} completed. <<<<< \n")
         
     except Exception as e:
         logger.exception(e)
-        
+        raise e
+    
     try:
         logger.info(f">>>>> Stage {STAGE_NAME2} started <<<<<")
-        obj = PrepareBaseModelTrainingPipeline()
-        obj.main()
+        prepare_base_model = PrepareBaseModelTrainingPipeline()
+        prepare_base_model.main()
         logger.info(f">>>>> Stage {STAGE_NAME2} completed. <<<<< \n")
     except Exception as e:
         logger.exception(e)
+        raise e
+    
+    try:
+        logger.info(f">>>>> Stage {STAGE_NAME3} started <<<<<")
+        model_trainer = ModelTrainingPipeline()
+        model_trainer.main()
+        logger.info(f">>>>> Stage {STAGE_NAME3} completed. <<<<< \n")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    try:
+        logger.info(f">>>>> Stage {STAGE_NAME4} started <<<<<")
+        model_evaluator = ModelEvaluationPipeline()
+        model_evaluator.main()
+        logger.info(f">>>>> Stage {STAGE_NAME4} completed. <<<<< \n")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    try:
+        logger.info(f">>>>> Stage {STAGE_NAME5} started <<<<<")
+        model_trainer = ModelInferencePipeline()
+        model_trainer.main()
+        logger.info(f">>>>> Stage {STAGE_NAME5} completed. <<<<< \n")
+    except Exception as e:
+        logger.exception(e)
+        raise e
