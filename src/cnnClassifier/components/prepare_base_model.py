db290ae29bc89b7295e9e0ed034e8a7a0e1ee67d
Amine MEKKI <amine.mekki@mines-ales.org>
2023-09-26 15:44:06 +0200

    fix bugs in the model definition

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -1,18 +1,19 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
 import torch
-import torch.nn as nn
-import torch.optim as optim
+from torch import nn, optim
+from pathlib import Path
+from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
 from torchvision import models
 from torchsummary import summary
-from pathlib import Path
-from cnnClassifier.constants import *
-from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
-
 
 class PrepareBaseModel:
     def __init__(self, config: PrepareBaseModelConfig):
         self.config = config
 
     def get_base_model(self):
+        # Note: torchvision has pre-trained weights only for include_top=True. So if you need exclude top, you'll need to handle that differently.
         self.model = models.vgg19(pretrained=(self.config.params_weights == "imagenet"))
 
         if not self.config.params_include_top:
@@ -37,21 +38,21 @@ class PrepareBaseModel:
             # Adding new layers
             model = nn.Sequential(
                 model,
-                nn.AdaptiveAvgPool2d((1, 1)),   # Adding Adaptive Average Pooling
                 nn.Flatten(),
-                nn.Linear(512, classes)  # Adjusted from 512 * 7 * 7 to just 512
+                nn.Linear(512 * 7 * 7, classes)  # This assumes the standard output size of VGG19 before classifier. Adjust if necessary.
             )
         else:
+            # Adjusting existing classifier for different number of classes
             num_ftrs = model.classifier[6].in_features
             model.classifier[6] = nn.Linear(num_ftrs, classes)
 
         optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=learning_rate)
         criterion = nn.CrossEntropyLoss()
 
-        return model, optimizer, criterion
+        return model
 
     def update_base_model(self):
-        self.full_model, optimizer, criterion = self._prepare_full_model(
+        self.full_model = self._prepare_full_model(
             model=self.model,
             classes=self.config.params_classes,
             freeze_all=True,
