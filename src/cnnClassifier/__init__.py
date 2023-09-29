40807744674ee5299587cda6cd322bee609c6856
Amine MEKKI <amine.mekki@mines-ales.org>
2023-09-29 11:35:55 +0200

    fixing log issue

--- src/cnnClassifier/__init__.py
+++ src/cnnClassifier/__init__.py
@@ -5,25 +5,26 @@ Date : 08/31/2023
 
 Description : This script is used to initialize the logger for the project.
 """
-
 import os 
 import sys
 import logging 
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE[:-4])
+logs_path = os.path.join(os.getcwd(), 'logs')
 
 os.makedirs(logs_path, exist_ok=True)
 
+LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y')}.log"
+
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
 logging_str = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"
 
 logging.basicConfig(
-    format = logging_str,
-    level= logging.INFO, 
-    handlers = [
-        logging.FileHandler(LOG_FILE_PATH),
+    format=logging_str,
+    level=logging.INFO, 
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH, mode='a'),  
         logging.StreamHandler(sys.stdout)
     ]
     )
